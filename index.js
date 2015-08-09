85242f803fa0f81020cfbbc7f64260d7cae65b78
Aaron <aaron@memoryjane.com>
2015-08-09 15:32:48 -0700

    Created index.js stub. Added event.json and package.json.

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * This is what gets called by Lambda with each Alexa interaction.
+ * @param event
+ * @param context
+ */
+exports.handler = function (event, context) {
+    console.log("6WordsIndex _handler  START");
+
+    // Create an instance of the SixWords skill and execute it.
+    // var memoryJane = new MemoryJane();
+    // memoryJane.execute(event, context);
+    console.log("6WordsIndex _handler_  DONE");
+};
