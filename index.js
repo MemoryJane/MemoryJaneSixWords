41153516b9ffe9d4ef35e568ccf8aba44a1ccdbb
Aaron <aaron@memoryjane.com>
2015-08-09 17:35:32 -0700

    Got rid of the event handlers - didn't need three layers. Request handlers and intent handlers are more than enough.

--- index.js
+++ index.js
@@ -2,51 +2,45 @@
  * Module for the Six Words functionality, based on the Alexa spec.
  */
 var sixWords = (function () {
+    // This data object is our connection to the database.
+    var data = require("./data.js");
 
+    // When a new session starts, initialize anything that needs initializing.
+    function onSessionStarted(sessionStartedRequest, session, context) {
+        // TODO Maybe fire up the DB here?
+    }
+
+    // Request handlers just pass off the
     var requestHandlers = {
         LaunchRequest: function (event, context) {
-            eventHandlers.onLaunch(event.request, event.session, context);
-        },
-        IntentRequest: function (event, context) {
-            eventHandlers.onIntent(event.request, event.session, context);
-        },
-        SessionEndedRequest: function (event, context) {
-            eventHandlers.onSessionEnded(event.request, event.session, context);
-        }
-    };
-
-    var eventHandlers = {
-        onSessionStarted: function (sessionStartedRequest, session, context) {
-            // TODO Maybe fire up the DB here?
-        },
-
-        onLaunch: function (launchRequest, session, context) {
             // Send a welcome message. Ask if the user wants to listen to a story.
             var welcomeMessage = "Welcome to Six Word Stories. ";
             welcomeMessage += "You can say listen to hear a sweet little six word story.";
             alexaAsk(welcomeMessage, context);
         },
-
-        onIntent: function (intentRequest, session, context) {
-            // TODO handle the intent
-            var intent = intentRequest.intent,
-                intentName = intentRequest.intent.name,
+        IntentRequest: function (event, context) {
+            var intent = event.request.intent,
+                intentName = event.request.intent.name,
                 intentHandler = intentHandlers[intentName];
             if (intentHandler) {
                 console.log('SixWords _onIntent dispatch intent = ' + intentName);
-                intentHandler(intent, session);
+                intentHandler(intent, event.session, context);
             } else {
                 throw 'SixWords ERROR Unsupported intent: ' + intentName;
             }
         },
-        onSessionEnded: function (sessionEndedRequest, session, context) {
+        SessionEndedRequest: function (event, context) {
             // TODO maybe clean up any DB here?
         }
     };
 
     var intentHandlers = {
-        ListenIntent: function (intent, session) {
-            // TODO
+        ListenIntent: function (intent, session, context) {
+            // Get a story from data.
+            data.getRandomStory(function (nextStory) {
+                // Read the story, Alexa.
+                alexaAsk(nextStory, context);
+            });
         }
     };
 
@@ -70,16 +64,19 @@ var sixWords = (function () {
         */
 
         context.succeed(alexaResponse);
-    };
+    }
 
 
     return {
+        /*
+         * The only public function of the module. Called by the handler, it executes the Alexa Skill.
+         */
         execute: function(event, context) {
-            // TODO: Do we want to check out AppID here?
+            // TODO: Do we want to check the AppID here?
 
             // If the session is new, initialize it.
             if (event.session.new) {
-                eventHandlers.onSessionStarted(event.request, event.session, context);
+                onSessionStarted(event.request, event.session, context);
             }
 
             // Route the request to the right handler.
