3edfe69642a48e8f114a8957e79dbeb7fd52d9ef
dmwilliams1995 <david@memoryjane.com>
2015-08-09 19:48:49 -0700

    Added to getRandomStory

--- data.js
+++ data.js
@@ -21,28 +21,17 @@ var data = (function () {
 
     return {
         getRandomStory: function (callback){
-            // Get the number of stories by doing a COUNT scan.
-            var dateTimeParams = { TableName: "MemoryJaneSixWordStories", Select: 'COUNT'};
-            dynamodb.scan(dateTimeParams, function (dateTimeErr, dateTimeData) {
-                if (dateTimeErr) console.log("Data _tableCount_  ERROR " + dateTimeErr);
-
-                var date = "09082015";
-                var time = "0930348493049";
-
-                //console.log(dateTimeData.DateStamp.N);
-
-                console.log(date);
-                console.log(time);
-
-                var tableParams = {TableName: "MemoryJaneSixWordStories", Key: { DateStamp: {"N": date}, TimeStamp: {"N": time} } };
-                dynamodb.getItem(tableParams, function (tableStoryErr, tableStoryData) {
-                    if (tableStoryErr) console.log("Data _getRandomStory  ERROR " + tableStoryErr);
-                    else {
-                        var story = tableStoryData.Item.Story.S;
-                        console.log("Data _gettingStory_ " + story);
-                        callback(story);
-                    }
-                });
+            // Get all of the data from the MemoryJaneSixWordStories Table
+            var tableParams = { TableName: "MemoryJaneSixWordStories"};
+            dynamodb.scan(tableParams, function (tableStoryErr, tableStoryData) {
+                if (tableStoryErr) console.log("Data _tableScan_  ERROR " + tableStoryErr);
+                else {
+                    var storyCount = tableStoryData.Count;
+                    var randomStoryIndex = (Math.floor(Math.random() * storyCount));
+                    var story = tableStoryData.Items[randomStoryIndex].Story.S;
+                    console.log("Data _gettingStory_ " + story);
+                    callback(story);
+                }
             });
         }
     }
