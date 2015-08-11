81a1550bf68a4a5eaf1f723d8c68cde2ea51e643
Aaron <aaron@memoryjane.com>
2015-08-10 23:25:30 -0700

    FINALLY fixed the stupid DB to pull the local environment variable out of the .env file.

--- NodeLambdaWrapper.js
+++ NodeLambdaWrapper.js
@@ -1,8 +1,6 @@
 // Load the environment variables for the DB.
 require('dotenv').load();
 
-console.log(process.env);
-
 // Get the node-lambda module.
 var nodeLambda = require("../node-lambda/lib/main");
 
