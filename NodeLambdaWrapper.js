fa12120f2ffb5089ebb7be9ef18a99b76e6c5ee1
Aaron <aaron@memoryjane.com>
2015-08-15 19:55:20 -0700

    Made the wrapper understand the chain of tests in numbered test files, and made it pass the attributes between the tests so we can do full conversations now with our tests.

--- NodeLambdaWrapper.js
+++ NodeLambdaWrapper.js
@@ -1,21 +1,32 @@
 // Load the environment variables for the DB.
 require('dotenv').load();
+var fs = require('fs-extra');
 
 // Get the node-lambda module. Override a couple methods to get a callback when the app completes.
 var nodeLambda = require("../node-lambda/lib/main");
-nodeLambda.run = function (program, callback) {
+nodeLambda.run = function (program, runWithTheseAttributes, callback) {
     this._createSampleFile('event.json');
     var splitHandler = program.handler.split('.');
     var filename = splitHandler[0] + '.js';
     var handlername = splitHandler[1];
     var handler = require(process.cwd() + '/' + filename)[handlername];
-    var event = require(process.cwd() + '/event.json');
+    var event = fs.readJSONSync(process.cwd() + '/event.json');
+
+    // If there are some attributes passed in, they're from the previous test, so add them to the event.
+    if (runWithTheseAttributes) {
+        console.log(event);
+        event.session.attributes = runWithTheseAttributes;
+        console.log(event);
+    }
+
     this._runHandler(handler, event, callback);
 };
 nodeLambda._runHandler = function (handler, event, callback) {
     var context = {
         succeed: function (result) {
-            callback(true);
+            // Keep the session attributes alive, in case there is another test coming.
+            var sessionAttributes = result.sessionAttributes;
+            callback(true, sessionAttributes);
         },
         fail: function (error) {
             callback(false);
@@ -25,18 +36,16 @@ nodeLambda._runHandler = function (handler, event, callback) {
 };
 
 // This is a recursive function to call nodeLambda run on a series of test files.
-function runTests(filename, number) {
+function runTests(filename, number, runWithTheseAttributes) {
     console.log("\n");
-    var fs = require('fs-extra');
     try {
         fs.copySync(filename.replace("#",number), './event.json');
-        nodeLambda.run({"handler":"index.handler"}, function(success) {
+        nodeLambda.run({"handler":"index.handler"}, runWithTheseAttributes, function(success, sessionAttributes) {
             // If this is a series, call this recursively until I run out of numbered files.
-            if(filename.search("#") != -1) runTests(filename, number+1);
+            if(filename.search("#") != -1) runTests(filename, number+1, sessionAttributes);
         });
     } catch (error) {
         console.log("\n No more tests.");
-        console.log(error);
     }
 }
 
@@ -53,7 +62,7 @@ for (i = 0; i < process.argv.length; i++) {
 // Did I find a filename?
 if (eventFileName) {
     // Yes, run the tests. If this is a series, it will automatically call recursively to run all of the tests.
-    runTests(eventFileName, 1);
+    runTests(eventFileName, 1, null);
 } else {
     // No file name provided, so no copy needed, just use the standard ./event.json.
     nodeLambda.run({"handler":"index.handler"});
